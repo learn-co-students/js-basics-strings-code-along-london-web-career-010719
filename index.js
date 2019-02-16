8b45a255a7033098baec7f5d9dbb1b25dad3b23d
Chris Camacho <cacamacho91@gmail.com>
2019-02-16 19:26:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = 'Chris Camacho';
+
+let welcomeSuffix = currentUser + '!';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + welcomeSuffix
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = 'Welcome, ' + currentUser.substr(0,1) + '!'
